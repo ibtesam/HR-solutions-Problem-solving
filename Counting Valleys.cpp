473d95063f832d2d9986b3164b0264e1cd14bb75
Ibtesam <ibtesamshakil@gmail.com>
2021-03-07 15:57:15 +0500

    bool variable (InValley) has been removed

--- Counting Valleys.cpp
+++ Counting Valleys.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int countingValleys(int steps, string path) {
     int seaLvL = 0, valleyVisited = 0;
-    bool InValley = false;
     for (int i = 0; i < steps; i++) {
         if (path[i] == 'U') {
             seaLvL++;
@@ -14,12 +13,8 @@ int countingValleys(int steps, string path) {
         else {
             seaLvL--;
         }
-        if (seaLvL == -1) {
-            InValley = true;
-        }
-        if (seaLvL == 0 && InValley == true) {
+        if (seaLvL == 0 && path[i] == 'U') {
             valleyVisited++;
-            InValley = false;
         }
     }
     return valleyVisited;
